d6d43bb7b58b166ae51d670916dba23e1c852178
Kevin Sylvestre <kevin@ksylvest.com>
2020-06-25 09:13:50 -0700

    Bump dependencies to latest version

--- spec/dummy/config/initializers/other.rb
+++ spec/dummy/config/initializers/other.rb
@@ -1,8 +1,6 @@
 Rails.application.config.action_dispatch.cookies_serializer = :json
-Rails.application.config.session_store :cookie_store, key: '_picturesque_session'
-
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json] if respond_to?(:wrap_parameters)
+  wrap_parameters format: [:json]
 end
 
 Rails.application.config.filter_parameters += [:password]
